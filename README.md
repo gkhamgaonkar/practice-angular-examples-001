0cecca3977ee5e6b66f740ac6fd650d7f1a13a2b
Gunjan <g.khamgaonkar@gmail.com>
2017-05-17 09:54:14 +1000

    updated code

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 ## Introduction
-This project aims to capture examples,which i encountered while learning Angular. It currenly also has examples from Typescript etc. which people may find useful 
+This project aims to capture examples,which i encountered while learning Angular. It currenly also has examples from Typescript etc. which people may find useful
 ### Typescript Examples
+Although corresponding js is being provided with the Typescript , the readers are encouraged to run build and run the type scripts
+[Link to install typescript and compile code.](https://www.typescriptlang.org/docs/handbook/ts-in-5.html#installing-typescript)   
 #### Functions 
- **File Name** : type_scripts\00_Functions.ts
+**File Name** : type_scripts\00_Functions.ts
 ###### (Rest Parameters) 
 1. The method passes variable argument to the method
 1. optional pameters 
@@ -18,3 +20,7 @@ This project aims to capture examples,which i encountered while learning Angular
 ###### Arrays
 1. Functions returning arrays
 2. Functions passing arrays
+#### Tuples Union Interface Classes and Objects 
+**File Name** : type_scripts\01_TuplesUnionInterfacesClassesObjects.ts
+
+
---
+++ type_scripts/01_TuplesUnionInterfacesClassesObjects.js
@@ -0,0 +1,59 @@
+/*
+ * Tuple is like array but they enable storing multiple fields of different types. Tuples can also be passed as parameters to functions.
+ * */
+var mytuple = [10, "Hello"];
+/*
+ * Function to concatenate values of Tuples and return as string
+ */
+function concatinateTuples(mytuple) {
+    var retValue = "";
+    for (var val = 0; val < mytuple.length; val++) {
+        retValue = retValue + ((val == 0) ? "" : ",") + mytuple[val];
+    }
+    return retValue;
+}
+/*
+ * Function to add item to tuple
+ */
+function addItemToTuple(myItem) {
+    mytuple.push(myItem);
+}
+/*
+ * Function popout Last Value From Tuple
+ */
+function popoutLastValueFromTuple() {
+    return mytuple.pop();
+}
+/*
+ * Anonymous Functions - 02 calculate square
+ * Utility function : to log data
+ */
+var log = function (logData, logType) {
+    if (logType === void 0) { logType = "DEBUG"; }
+    if (logType == null || logType === undefined || logType == "DEBUG") {
+        console.log("DEBUG LOG : " + logData);
+    }
+    else if (logType == "ERROR") {
+        console.error("ERROR LOG : " + logData);
+    }
+    else if (logType == "WARN") {
+        console.warn("WARN LOG : " + logData);
+    }
+    else if (logType == "INFO") {
+        console.info("INFO LOG : " + logData);
+    }
+};
+/***
+ * runs with different values
+ *
+ */
+log("==============(Tuple)==============");
+log("Concatinate Touples : " + concatinateTuples(mytuple));
+log("add item to tuple");
+addItemToTuple("myWorld");
+addItemToTuple(1123);
+log("New Touple items : " + concatinateTuples(mytuple));
+log("popped value : " + popoutLastValueFromTuple());
+log("New Touple items : " + concatinateTuples(mytuple));
+log("popped value : " + popoutLastValueFromTuple());
+log("New Touple items : " + concatinateTuples(mytuple));
